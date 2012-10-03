f04f873f56ed439f16a60377184a9a1be8249880
Diego Peinado <dpeinad@gmail.com>
2012-10-03 15:01:54 +0200

    pitps

--- sql_creacion_db/query_q.sql
+++
@@ -1,7 +0,0 @@
-use proyectos;
-select nombre, descripcion, fase, segundos, creation_time, update_time 
-from recursos, Proyectos, fases, Cargas 
-where recursos.Idrecurso=Cargas.IdRecurso 
-and Proyectos.idProyecto=Cargas.idProyecto 
-and fases.idfase = Cargas.idfase
-order by Cargas.IdRecurso; 
