ea52a103aef7212bf77e5331f1de5886f48dcc39
Diego Peinado Martin <diego.peinado@intrame.com>
2012-11-05 14:17:50 +0100

    accc

--- es.estec.gestionProjectosV02/src/Servidor/projectsDataModel.py
+++ es.estec.gestionProjectosV02/src/Servidor/projectsDataModel.py
@@ -6,20 +6,6 @@ Created on 29/10/2012
 '''
 import MySQLdb
 
-#===============================================================================
-# class Project(object):
-#    '''
-#    Clase que representa una fila en la tabla de proyectos
-#    '''
-#    def __init__(self,IdP,IdPP,lft,rgt,Code,Description):
-#        self.IdP=IdP
-#        self.IdPP=IdPP
-#        self.lft=lft
-#        self.rgt=rgt
-#        self.Code=Code
-#        self.Description=Description
-#===============================================================================
-
 class ProjectTree(object):
     '''
     Clase que encapsula la estructura de datos del árbol de proyectos
@@ -31,11 +17,13 @@ class ProjectTree(object):
         self.conn = connDB
         self.myProjects={}
         self.projFromLft = {}
-        self.projFromRgt = {}
+        self.rgtFromLft = {}
     def updateTree(self):
         msgStr="""SELECT IdProject, IdProjectParent, lft, rgt , Code, Description FROM projects 
                     ORDER BY lft ASC"""
         self.myProjects={}
+        self.projFromLft={}
+        self.rgtFromLft={}
         try:
             cur=self.conn.cursor()
             cur.execute(msgStr)
@@ -45,7 +33,7 @@ class ProjectTree(object):
                 myP=[row[0],row[1],row[2],row[3],row[4],row[5]]
                 self.myProjects[row[0]]=myP
                 self.projFromLft[row[2]]=row[0]
-                self.projFromRgt[row[3]]=row[0]                
+                self.rgtFromLft[row[2]]=row[3]                
         except MySQLdb.Error, e:
             print "Error {0}".format(e)
     def insertNode(self,code,description,parentID):
@@ -58,96 +46,60 @@ class ProjectTree(object):
             cmpVal = row[1]-1
             msgStr="UPDATE projects SET rgt = rgt + 2 WHERE rgt > {0}".format(cmpVal)
             cur.execute(msgStr)
+            self.conn.commit()
             msgStr="UPDATE projects SET lft = lft + 2 WHERE lft > {0}".format(cmpVal)
             cur.execute(msgStr)
+            self.conn.commit()
             msgStr="""INSERT INTO projects(IdProjectParent,lft,rgt,Code,Description) 
                         VALUES({0},{1},{2},'{3}','{4}')""".format(parentID,row[1],row[1]+1,code,description)
             cur.execute(msgStr)                                    
-            cur.close()
             self.conn.commit()
-            self.updateTree()
+            cur.close()      
         except MySQLdb.Error, e:
             print "Error {0}".format(e)    
-                 
+            self.conn.rollback()     
+    def displayChildren(self,root):
+        lft=self.myProjects[root][2]
+        rgt=self.myProjects[root][3]
+        myKys=self.projFromLft.keys()
+        myKeys=[e for e in myKys if (e>=lft and e<=rgt)]
+        right=[]
+        for key in myKeys:
+            row=self.myProjects[self.projFromLft[key]]
+            if (len(right)>0):
+                while right[-1]<row[2]:
+                    right.pop()
+            msgStr=' '*5*len(right)+"{0} {1}: {2}".format(row[0],row[4],row[5])
+            print msgStr
+            right.append(row[3])
+    def path_node(self,node):
+        lft=self.myProjects[node][2]
+        rgt=self.myProjects[node][3]
+        myKys=self.projFromLft.keys()
+        myKeys=[e for e in myKys if (e<lft and self.rgtFromLft[e]>rgt)]
+        pathnode=[self.projFromLft[e] for e in myKeys]
+        pathnode.append(node)        
+        return pathnode
+    def numberDescendents(self,node):
+        lft=self.myProjects[node][2]
+        rgt=self.myProjects[node][3]
+        return (rgt-lft-1)/2        
+    def getLeaves(self):
+        myKys=self.projFromLft.keys()        
+        myKeys=[self.projFromLft[e] for e in myKys if (self.rgtFromLft[e]==e+1)]                
+        return myKeys
+    
 if __name__ == "__main__":
     myDB=MySQLdb.connect(host = 'localhost', user = 'puser', passwd = 'pu8549', db = 'proj',charset="utf8",use_unicode=True)
     myProjs = ProjectTree(myDB)
     myProjs.updateTree()
-    myProjs.insertNode('PR001', 'OTRA TAREA COLGANDO DE PR001', 50)
-    myKys = myProjs.projFromLft.keys()
-    for lft in myKys:
-        myID=int(myProjs.projFromLft[lft])
-        print lft,myID,myProjs.myProjects[myID]
-    pass        
-#===============================================================================
-#    def display_children(self,root):
-#        try:
-#            cur = self.conn.cursor() 
-#            # retrieve left and right value of the root node
-#            msgStr="SELECT lft, rgt FROM projects where Idproject = {}".format(root)        
-#            cur.execute(msgStr)   
-#            row = cur.fetchone()
-#            # start with an empty right stack
-#            right = [];
-#            # retrieve all descendants of the root node
-#            msgStr="""SELECT IdProject, lft, rgt , Code, Description FROM projects WHERE
-#                        lft BETWEEN {0} AND {1} ORDER BY lft ASC""".format(row[0],row[1])
-#            cur.execute(msgStr)
-#            data = cur.fetchall()
-#            cur.close()
-#            for row in data:
-#                if(len(right)>0):
-#                    while right[-1] < row[1]:
-#                        right.pop()
-#                msgStr=' '*5*len(right)+ "{0} {1}: {2}".format(row[0], row[3],row[4])
-#                print msgStr
-#                right.append(row[2])
-#        except MySQLdb.Error, e:
-#            print "Error {0}".format(e)
-#            
-#    def path_node(self,node):
-#        try:
-#            cur = self.conn.cursor() 
-#            # retrieve left and right value of the root node
-#            msgStr="SELECT lft, rgt FROM projects where Idproject = {}".format(node)        
-#            cur.execute(msgStr)   
-#            row = cur.fetchone()
-#            msgStr="""SELECT IdProject FROM projects WHERE
-#                        lft < {0} AND rgt > {1} ORDER BY lft ASC""".format(row[0],row[1])
-#            cur.execute(msgStr)
-#            data = cur.fetchall()
-#            cur.close()
-#            pathnode=[]           
-#            for row in data:
-#                pathnode.append(row[0])
-#            pathnode.append(node)
-#            cur.close()
-#            return pathnode
-#        except MySQLdb.Error, e:
-#            print "Error {0}".format(e)
-#    def numberDescendents(self,node):
-#        try:
-#            cur = self.conn.cursor() 
-#            # retrieve left and right value of the root node
-#            msgStr="SELECT lft, rgt FROM projects where Idproject = {}".format(node)        
-#            cur.execute(msgStr)   
-#            row = cur.fetchone()
-#            cur.close()        
-#            return (row[1]-row[0]-1)/2                                    
-#        except MySQLdb.Error, e:
-#            print "Error {0}".format(e)                                    
-# 
-#    def insertNode_AL(self,code,description,parentID):
-#        try:
-#            cur = self.conn.cursor()
-#            msgStr="INSERT INTO projects(Code,Description,IdProjectParent) VALUES('{0}','{1}',{2})".format(code,description,parentID)
-#            cur.execute(msgStr)            
-#            cur.close()
-#            #self.conn.commit()            
-#            self.rebuild_tree(1, 1)
-#        except MySQLdb.Error, e:
-#            print "Error {0}".format(e)
-#            
+    myProjs.displayChildren(34)
+    myProjs.path_node(76L)
+    print myProjs.numberDescendents(34)
+    print myProjs.getLeaves()
+
+                        
+            
 #    def getLeaves(self):
 #        try:
 #            cur = self.conn.cursor() 
@@ -163,43 +115,3 @@ if __name__ == "__main__":
 #        except MySQLdb.Error, e:
 #            print "Error {0}".format(e)    
 #            return [None,]
-#    def insertNode(self,code,description,parentID):
-#        try:
-#            cur = self.conn.cursor() 
-#            # retrieve left and right value of the root node
-#            msgStr="SELECT lft, rgt FROM projects where Idproject = {}".format(parentID)        
-#            cur.execute(msgStr)   
-#            row = cur.fetchone()
-#            cmpVal = row[1]-1
-#            msgStr="UPDATE projects SET rgt = rgt + 2 WHERE rgt > {0}".format(cmpVal)
-#            cur.execute(msgStr)
-#            msgStr="UPDATE projects SET lft = lft + 2 WHERE lft > {0}".format(cmpVal)
-#            cur.execute(msgStr)
-#            msgStr="""INSERT INTO projects(IdProjectParent,lft,rgt,Code,Description) 
-#                        VALUES({0},{1},{2},'{3}','{4}')""".format(parentID,row[1],row[1]+1,code,description)
-#            cur.execute(msgStr)                                    
-#            cur.close()
-#        except MySQLdb.Error, e:
-#            print "Error {0}".format(e)            
-#                                                        
-#    def rebuild_tree(self, parent, left):
-#        # the right value of this node is the left value + 1
-#        # ahora mismo este valor no es el de right. Al final de la recurrencia lo ser�. Ahora es solo
-#        # El valor left del pr�ximo nivel    
-#        right = left+1  
-#        # get all children of this node
-#        try:
-#            cur = self.conn.cursor() 
-#            msgStr="SELECT IdProject FROM projects where IdprojectParent = {}".format(parent)        
-#            cur.execute(msgStr)   
-#            result = cur.fetchall()
-#            for row in result:
-#                right = self.rebuild_tree(row[0],right) 
-#            msgStr="UPDATE projects SET lft = {0}, rgt = {1} WHERE IdProject = {2}".format(left,right,parent)
-#            cur.execute(msgStr)
-#            #self.conn.commit()
-#            cur.close()        
-#        except MySQLdb.Error, e:
-#            print "Error {0}".format(e)
-#        return right + 1   
-#===============================================================================
